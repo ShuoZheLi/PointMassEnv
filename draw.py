b09d61cff21efa9b5703760a58a19739f780f604
ShuoZheLi <lishuozhe1999@163.com>
2024-05-16 04:12:02 -0500

    first commit

---
+++ draw.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the array
+grid = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+])
+
+# Create the plot
+plt.figure(figsize=(8, 8))
+extent = [0, grid.shape[1], 0, grid.shape[0]]
+plt.imshow(grid, cmap='Greys', interpolation='none', extent=extent)
+
+
+
+# For demonstration, generate random trajectories
+# np.random.seed(0)
+
+traj = np.load('traj.npy')
+# plot the trajectory
+# for i in range(len(traj)):
+
+plt.plot(traj[:,1], traj[:,0], color='purple', alpha=1)
+
+
+plt.grid(color='gray', linestyle='-', linewidth=0.25)
+
+# change the size of the grid box to be 1x1
+plt.xticks(np.arange(0, 19, 1))
+plt.yticks(np.arange(0, 19, 1))
+
+# plt.xticks(np.arange(1, 19, 0.5))
+# plt.yticks(np.arange(1, 19, 0.5))
+# plt.xticks(np.arange(1, 19, 0.75))
+# plt.yticks(np.arange(1, 19, 0.75))
+plt.grid(True)
+# plt.gca().set_xticklabels([])
+# plt.gca().set_yticklabels([])
+# plt.gca().set_xticks([x - 0.75 for x in range(19)], minor=True)
+# plt.gca().set_yticks([y - 0.75 for y in range(19)], minor=True)
+
+
+# plt.axis('off')
+plt.title('Four-room')
+plt.gca().invert_yaxis()
+plt.show()
